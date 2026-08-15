ef5441cbd1b38a15cc1b99785c0d825e0fa70d07
Praveen Sinha <praveensinha.tech@gmail.com>
2026-08-16 00:34:01 +0530

    added resource terraform vars

--- main.tf
+++ main.tf
@@ -1,13 +1,6 @@
 resource "azurerm_resource_group" "rg1" {
+  for_each = var.rgs
 
-  name     = "rg-github-actions-demo"
-  location = "East US"
-
-}
-
-resource "azurerm_resource_group" "rg2" {
-
-  name     = "rg-github-actions-demo1"
-  location = "East US"
-
-}
+  name     =each.value.name
+  location = each.value.location
+  }
